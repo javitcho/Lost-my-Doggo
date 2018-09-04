2da280959fb59ed02b164e7492855ed0e1bb0e38
javitcho <42880814+javitcho@users.noreply.github.com>
2018-09-04 10:07:47 -0600

    Add files via upload

--- Mascota.java
+++ Mascota.java
@@ -1,4 +1,68 @@
 package com.progra.grupo.lostmydoggo;
 
+import android.media.Image;
+
 public class Mascota {
+    private String nombre;
+    private String descripcion;
+    private int id;
+    private String color;
+    private Image imagen;
+    private int idDueno;
+
+    public Mascota(int id,String nombre, String descripcion, String color, Image imagen, int idDueno){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion=descripcion;
+        this.color = color;
+        this.imagen = imagen;
+        this.idDueno=idDueno;
+    }
+    public int getIdDueno() {
+        return idDueno;
+    }
+
+    public void setIdDueno(int idDueno) {
+        this.idDueno = idDueno;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Image getImagen() {
+        return imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setImagen(Image imagen) {
+        this.imagen = imagen;
+    }
 }
--- Usuario.java
+++ Usuario.java
@@ -1,4 +1,47 @@
 package com.progra.grupo.lostmydoggo;
 
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String contrasena;
+    private int telefono;
+
+    public boolean validar(String nombre, String contrasena){
+        if(this.nombre.equals(nombre) && this.contrasena.equals(contrasena)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public int getId() {
+        return id;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
